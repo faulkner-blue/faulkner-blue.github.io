2cbd8d627d08ed2709ef629814d02935bbed5046
Faulkner Bodbyl-Mast <faulkner.blue@gmail.com>
2024-03-27 14:53:21 -0500

    lets try this

--- docs/assets/scripts/sketch.js
+++ docs/assets/scripts/sketch.js
@@ -4,16 +4,28 @@ let table = [208,45,100,
              240,0,94,
              96,47,85,
              107,46,60];
+             //28, 47, 73]; brown
 
-const pageW = document.getElementById("body").scrollWidth;
-const pageH = document.getElementById("body").scrollHeight * 1.25;
+
+var pageW = 0;  // document.getElementById("body").scrollWidth;
+var pageH = 0; //document.getElementById("body").scrollHeight * 1.25;
 
 function setup() {
-    
+    reset();
+
+}
+
+//THE JUICE
+function reset() {
+    pixelDensity(1);
+   pageW = windowWidth;
+   pageH = windowHeight;
   var myCanvas = createCanvas(pageW,pageH); //document.getElementById("body").scrollWidth, document.getElementById("body").scrollHeight, WEBGL);
-//  myCanvas.parent("wallpaper");
+  myCanvas.parent("wallpaper");
   myCanvas.position(0,0);
-  myCanvas.style('z-index','-1');    
+  myCanvas.style('z-index','-1');
+  myCanvas.elt.style.position = "fixed";  
+    
   background(255);
   angleMode(DEGREES);
   colorMode(HSB, 360, 100, 100, 255);
@@ -21,25 +33,27 @@ function setup() {
   var shapeCount = 25;
   var mod = (random(2,shapeCount));
    
-  drawingContext.filter = 'blur(15px)';
+//drawingContext.filter = 'blur('15px)';
   for(let i = 0; i < shapeCount; i++){ 
     push();
+    drawingContext.filter = 'blur('+str(random(2,40))+'px)';
     translate(random(0, pageW),random(0, pageH));
     rotate(random(0,360));
-    let square = new shape(i,mod);
-          
+    let square = new shape(i,mod);    
     pop();
   }
-    
 }
 
+function windowResized() {
+    reset();
+}
 
 class shape {
   constructor(id,mod) {
 //    this.x = random(0, pageW);
 //    this.y = random(0, pageH);
-    this.w = random(pageW*.025, pageW/1.25);
-    this.h = random(pageW*.025, pageH/1.5);
+    this.w = random(pageW*.01, pageW/1.25);
+    this.h = random(pageW*.01, pageH/1.5);
     
     id = id % (table.length / 3);  
     noStroke();
